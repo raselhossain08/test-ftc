8fa6b62f419ab0d084bf10c1cff9dfcfca8329af
rahatulslam <rahatulislam67777@gmail.com>
2024-09-21 05:08:06 +0600

    success

---
+++ src/config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: "dirdbnxe1",
+    api_key: "424511362532329",
+    api_secret: "pcuAYMOb9rJ0wC2uhbDDGkjE3Mc",
+});
+
+module.exports = cloudinary;
