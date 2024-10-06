772bb39198559b9f6415814f2a8d388519669030
rahatulslam <rahatulislam67777@gmail.com>
2024-10-06 21:33:21 +0600

    limit

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ connectDB();
 
 // Middleware
 app.use(cors()); // Enable CORS
-app.use(express.json()); // Built-in body parser for JSON
 app.use(bodyParser.urlencoded({ extended: true })); // To handle URL-encoded form data
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // Serve static files from the uploads directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
